08f1ddc16e9921bbb426e85657dc244ade990c16
XLuoChen <1425145741@qq.com>
2016-06-26 16:30:56 +0800

    get next id

--- next-id.js
+++ next-id.js
@@ -1,7 +1,15 @@
-var id = 0;
+var fs = require('fs');
+var id;
+
+fs.readFile('./id.json', 'UTF-8', function (err, data) {
+
+    if (err) return;
+    id = JSON.parse(data).id;
+});
 
 module.exports = function () {
 
     id += 1;
+    fs.writeFile('./id.json', JSON.stringify({"id": id}));
     return id;
 };
